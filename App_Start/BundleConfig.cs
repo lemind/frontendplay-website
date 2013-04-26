56f375de72a46d6924622f079a719da2b5b45725
ceee <cee@live.at>
2013-04-26 18:25:18 +0200

    add empty mvc4 template;

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace frontendplay
+{
+  public class BundleConfig
+  {
+    // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+      bundles.Add(new ScriptBundle("~/bundles/headjavascript").Include(
+
+      ));
+
+      bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
+        "~/Content/Scripts/app.js"
+      ));
+
+      bundles.Add(new StyleBundle("~/bundles/stylesheet").Include(
+        "~/Content/Sass/app.css"
+      ));
+    }
+  }
+}
