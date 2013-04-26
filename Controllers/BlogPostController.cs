271ecf7c5b6de829447c185e714083b7b858d24a
ceee <cee@live.at>
2013-04-26 21:28:52 +0200

    database context; blogPost Model; enable migrations;

---
+++ Controllers/BlogPostController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using frontendplay.Models;
+
+namespace frontendplay.Controllers
+{
+    public class BlogPostController : Controller
+    {
+      private StoreContext db = new StoreContext();
+
+
+
+      // GET: /BlogPost/
+      public ActionResult Index()
+      {
+          return View(db.BlogPostModels.ToList());
+      }
+
+
+
+      // GET: /BlogPost/Details/5
+      public ActionResult Details(int id = 0)
+      {
+          BlogPostModel blogpostmodel = db.BlogPostModels.Find(id);
+          if (blogpostmodel == null)
+          {
+              return HttpNotFound();
+          }
+          return View(blogpostmodel);
+      }
+
+
+
+      protected override void Dispose(bool disposing)
+      {
+          db.Dispose();
+          base.Dispose(disposing);
+      }
+    }
+}
---
+++ Models/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace frontendplay.Models
+{
+  public class StoreContext : DbContext
+  {
+    public StoreContext()
+      : base("DefaultConnection")
+    {
+
+    }
+
+    public DbSet<BlogPostModel> BlogPostModels { get; set; }
+
+    public DbSet<UserProfile> UserProfiles { get; set; }
+  }
+}
