8af3b2ddb10b1dd73dc3a3f6ba6ee7197d4430f1
ceee <cee@live.at>
2013-04-27 14:13:26 +0200

    use stackoverflow friendlyUrl implementation - works like a charm;

--- Utilities/UrlEncoder.cs
+++ Utilities/UrlEncoder.cs
@@ -9,38 +9,135 @@ namespace frontendplay.Utilities
 {
   public class UrlEncoder
   {
-    public static string ToFriendlyUrl(string urlToEncode)
+    public static string ToFriendlyUrl(string title)
     {
-      urlToEncode = (urlToEncode ?? "").Trim().ToLower();
+      if (title == null) return "";
 
-      StringBuilder url = new StringBuilder();
+      const int maxlen = 80;
+      int len = title.Length;
+      bool prevdash = false;
+      var sb = new StringBuilder(len);
+      char c;
 
-      foreach (char ch in urlToEncode)
+      for (int i = 0; i < len; i++)
       {
-        switch (ch)
+        c = title[i];
+        if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
         {
-          case ' ':
-            url.Append('-');
-            break;
-          case '&':
-            url.Append("and");
-            break;
-          case '\'':
-            break;
-          default:
-            if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z'))
-            {
-              url.Append(ch);
-            }
-            else
-            {
-              url.Append('-');
-            }
-            break;
+          sb.Append(c);
+          prevdash = false;
         }
+        else if (c >= 'A' && c <= 'Z')
+        {
+          // tricky way to convert to lowercase
+          sb.Append((char)(c | 32));
+          prevdash = false;
+        }
+        else if (c == ' ' || c == ',' || c == '.' || c == '/' ||
+            c == '\\' || c == '-' || c == '_' || c == '=')
+        {
+          if (!prevdash && sb.Length > 0)
+          {
+            sb.Append('-');
+            prevdash = true;
+          }
+        }
+        else if ((int)c >= 128)
+        {
+          int prevlen = sb.Length;
+          sb.Append(RemapInternationalCharToAscii(c));
+          if (prevlen != sb.Length) prevdash = false;
+        }
+        if (i == maxlen) break;
       }
 
-      return url.ToString();
-    } 
+      if (prevdash)
+        return sb.ToString().Substring(0, sb.Length - 1);
+      else
+        return sb.ToString();
+    }
+
+
+
+    public static string RemapInternationalCharToAscii(char c)
+    {
+      string s = c.ToString().ToLowerInvariant();
+      if ("àåáâäãåą".Contains(s))
+      {
+        return "a";
+      }
+      else if ("èéêëę".Contains(s))
+      {
+        return "e";
+      }
+      else if ("ìíîïı".Contains(s))
+      {
+        return "i";
+      }
+      else if ("òóôõöøőð".Contains(s))
+      {
+        return "o";
+      }
+      else if ("ùúûüŭů".Contains(s))
+      {
+        return "u";
+      }
+      else if ("çćčĉ".Contains(s))
+      {
+        return "c";
+      }
+      else if ("żźž".Contains(s))
+      {
+        return "z";
+      }
+      else if ("śşšŝ".Contains(s))
+      {
+        return "s";
+      }
+      else if ("ñń".Contains(s))
+      {
+        return "n";
+      }
+      else if ("ýÿ".Contains(s))
+      {
+        return "y";
+      }
+      else if ("ğĝ".Contains(s))
+      {
+        return "g";
+      }
+      else if (c == 'ř')
+      {
+        return "r";
+      }
+      else if (c == 'ł')
+      {
+        return "l";
+      }
+      else if (c == 'đ')
+      {
+        return "d";
+      }
+      else if (c == 'ß')
+      {
+        return "ss";
+      }
+      else if (c == 'Þ')
+      {
+        return "th";
+      }
+      else if (c == 'ĥ')
+      {
+        return "h";
+      }
+      else if (c == 'ĵ')
+      {
+        return "j";
+      }
+      else
+      {
+        return "";
+      }
+    }
   }
 }
