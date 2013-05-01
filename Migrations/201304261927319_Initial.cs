c66ff41d06e7e51196809c2dcc90f1b82e450ff6
ceee <cee@live.at>
2013-05-01 19:10:33 +0200

    add output cache

--- Migrations/201304261927319_Initial.cs
+++ Migrations/201304261927319_Initial.cs
@@ -19,20 +19,10 @@ namespace frontendplay.Migrations
                     })
                 .PrimaryKey(t => t.ID);
             
-            CreateTable(
-                "dbo.UserProfile",
-                c => new
-                    {
-                        UserId = c.Int(nullable: false, identity: true),
-                        UserName = c.String(),
-                    })
-                .PrimaryKey(t => t.UserId);
-            
         }
         
         public override void Down()
         {
-            DropTable("dbo.UserProfile");
             DropTable("dbo.BlogPostModels");
         }
     }
