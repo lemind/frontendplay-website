dda2e7739357fe5721a4da6325924159e9c71652
ceee <cee@live.at>
2013-05-27 21:06:49 +0200

    add piwik;

--- Content/Scripts/app.js
+++ Content/Scripts/app.js
@@ -501,3 +501,14 @@ Prism.languages.insertBefore('coffeescript', 'keyword', {
 
   'attr-name': /[_?a-z-|A-Z-]+(\s*:)| @[_?$?a-z-|A-Z-]+(\s*)| /g
 });
+
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+	var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.ceecore.com//";
+	_paq.push(['setTrackerUrl', u+'piwik.php']);
+	_paq.push(['setSiteId', 1]);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+	g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})(); 
--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function(grunt)
     concat: {
       build: {
         src: [
-          'Content/Scripts/Vendor/prism/prism.js'
+          'Content/Scripts/Vendor/prism/prism.js',
+          'Content/Scripts/piwik.js'
         ],
         dest: 'Content/Scripts/app.js'
       }
